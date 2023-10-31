aee09181785e161676c386354f96a7dd0169e931
Ink-Link <114848243+Ink-Link@users.noreply.github.com>
2023-10-31 19:22:40 -0300

    Versão de 31/10

--- telinhas.py
+++ telinhas.py
@@ -1,26 +1,65 @@
 from tkinter import *
-from tkinter import ttk
+from PIL import ImageTk,Image
+from tkinter import filedialog
 
 def conectarServidor():
     pass
 
+def enviarImg():
+    pass
+
+def baixarImg():
+    pass
+
+def selecionarDl():
+    pass
+
+def selecionarUp():
+    global img
+    telaMain.filename = filedialog.askopenfilename(initialdir="/Users/PC/Downloads", title="Selecionar Imagem", filetypes=(("PNG files", "*.png"),("JPEG files","*.jpeg")))
+    img = ImageTk.PhotoImage(Image.open(telaMain.filename))
+    imgLabel = Label(image=img)
+    imgLabel.grid(column=1, row=2)
+
+################################# CÓDIGO PRINCIPAL ################################
+
 telaMain = Tk()
 telaMain.title("EzShare")
-#telaMain.geometry("400x200")
+#telaMain.geometry("790x660")
+status = "Offline"
 
-bio = Label(telaMain,text = "Faça upload e download de arquivos no servidor!")
-bio.grid(column=0, row=0)
+#Cria os textos informativos#
+bio = Label(telaMain,text = "Faça upload e download de arquivos no servidor! \n Status: " + status) #Header
+bio.grid(column=1, row=0)
 
-btConectar = Button(telaMain, text="Conectar ao Servidor", command=conectarServidor).grid(column=0, row=1)
+acao = Label(telaMain,text = "Aguardando Ação...") #Texto informando a ação tomada
+acao.grid(column=1, row=4)
 
-'''
-#Cria e dispôe os botões no display
-btnEnviarDl = Button(telaMain, text="Enviar").grid(column=0, row=1)
-btnSelecionarDl = Button(telaMain, text="Selecionar").grid(column=0, row=2)
+bio = Label(telaMain,text = "Upload")
+bio.grid(column=0, row=3)
 
-btnEnviarUp = Button(telaMain, text="Enviar").grid(column=2, row=1) ##pack(expand=1, side="right", padx=10, pady= 50)
-btnSelecionarUp = Button(telaMain, text="Selecionar").grid(column=2, row=2) ##pack(expand=1, side="right", padx=10, pady= 50)
-'''
+bio = Label(telaMain,text = "Download")
+bio.grid(column=2, row=3)
+
+
+#Cria todos os botões do menu principal#
+btnConectar = (Button(telaMain, text="Conectar ao Servidor", command=conectarServidor)) #Conectar ao servidor
+
+#Botões de download
+btnBaixarDl = Button(telaMain, text="Baixar", command=baixarImg)
+btnSelecionarDl = Button(telaMain, text="Selecionar", command=selecionarDl)
+
+#Botões de upload
+btnEnviarUp = Button(telaMain, text="Enviar", command=enviarImg)
+btnAbrirUp = Button(telaMain, text="Abrir", command=selecionarUp)
+
+
+#Dispõe os botões no display#
+btnConectar.grid(column=1, row=1)
+btnBaixarDl.grid(column=2, row=4)
+btnSelecionarDl.grid(column=2, row=5)
+btnEnviarUp.grid(column=0, row=4)
+btnAbrirUp.grid(column=0, row=5)
 
 telaMain.mainloop()
 
@@ -28,7 +67,7 @@ telaMain.mainloop()
 
 
 
-###################### CÓDIGO ANTIGO ########################
+################################# CÓDIGO ANTIGO #################################
 '''
 import tkinter as tk
 #from tkinter import ttk
